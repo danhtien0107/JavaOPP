b6f387528805b3d79259568404004dbbae4cb717
danhtien0107 <ndtien2k4@gmail.com>
2024-01-29 13:17:33 +0700

    LAB2

--- LAB2/src/Bai1.java
+++ LAB2/src/Bai1.java
@@ -1,36 +1,28 @@
 import java.util.Scanner;
 
 public class Bai1 {
-    public static void main(String[] args) {
-        // Khai báo biến
+    public static void main(String[] args) {        
         double num1, num2;
-
-        // Sử dụng Scanner để nhập số từ bàn phím
         Scanner scanner = new Scanner(System.in);
 
-        // Nhập số thứ nhất
         System.out.print("Nhap so thu nhat : ");
         num1 = scanner.nextDouble();
 
-        // Nhập số thứ hai
         System.out.print("Nhap so thu hai: ");
         num2 = scanner.nextDouble();
 
-        // Tính tổng, hiệu, tích, thương, chia lấy dư
         double sum = num1 + num2;
         double difference = num1 - num2;
         double product = num1 * num2;
         double quotient = num1 / num2;
         double remainder = num1 % num2;
 
-        // In kết quả
         System.out.println("Tong: " + sum);
         System.out.println("Hieu: " + difference);
         System.out.println("Tich: " + product);
         System.out.println("Thuong: " + quotient);
         System.out.println("Chia lay du: " + remainder);
 
-        // So sánh hai số
         if (num1 > num2) {
             System.out.println(num1 + " lon hon " + num2);
         } else if (num1 < num2) {
@@ -39,7 +31,6 @@ public class Bai1 {
             System.out.println("Hai so bang nhau");
         }
 
-        // Đóng Scanner
         scanner.close();
     }
 }
